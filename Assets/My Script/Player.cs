901bb68a24a03d62c565b9b9c89b60593278e067
kakeru0129 <82084029+kakeru0129@users.noreply.github.com>
2021-07-16 17:45:11 +0900

    アイテム獲得のプログラム

--- Assets/My Script/Player.cs
+++ Assets/My Script/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     //宣言でRigidbody型の変数『rb』を定義している
     //クラス内で使う変数は冒頭で宣言
     public float speed;
+    int count;
+    //今回は個数で必ず整数になるので『int型』を使う
 
     // Start is called before the first frame update
     //void Startやvoid UpdateはMethod(メソッド)という
@@ -18,6 +20,8 @@ public class Player : MonoBehaviour
     //ゲームの初期設定処理やタイトル画面の表示など
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        //初期値は0
     }
 
     // Update is called once per frameの戻り値
@@ -36,5 +40,19 @@ public class Player : MonoBehaviour
         //moveHは左右、moveVは前後
         rb.AddForce(move * speed);
         //rb.AddForce(move)は一番初めに変数rbに入れたPlayerのRigidbodyにmoveの力を加えなさいという命令
+    }
+
+    private void OnTriggerEnter(Collider other)
+    //OnTriggerEnter()メソッドは他のオブジェクトに接触した瞬間に実行
+    //このメソッドは引数を持ち、Collider型は文字通りColliderを代入することができる型で、そのときに触れた相手が自動的に変数otherに代入される
+    {
+        other.gameObject.SetActive(false);
+        //otherに代入されているColliderを持っているオブジェクト、つまり接触したゲームオブジェクトのSetActive()メソッドを呼び出して実行しなさいという意味。
+        //SetActive()メソッドはオブジェクトのアクティブ・非アクティブを制御するメソッドで、『bool型』と言ってtrueかfalseのどちらかの値だけしか代入できない型
+        //trueがアクティブ化、falseが非アクティブ化
+
+        count = count + 1;
+        //『今のcountの値に1を足してそれを新しいcountの値にしなさい』という意味で『count += 1』と書いてもいい
+        Debug.Log(count);
     }
 }
